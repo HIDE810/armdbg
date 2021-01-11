6c5b703a8d1c6fadbe10f59a1e27a9191c850cf0
HIDE810 <hidetake1231@gmail.com>
2021-01-12 01:03:58 +0900

    cleanup

--- armdbg.sh
+++ armdbg.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-arm-linux-gnueabi-as -g -o $1.o $1.s
-arm-linux-gnueabi-gcc -o $1.out $1.o
-rm $1.o
+arm-linux-gnueabi-gcc -o $1.out $1.s
 echo "Waiting for GDB ..."
 qemu-arm -singlestep -g 1234 -L /usr/arm-linux-gnueabi/ $1.out
